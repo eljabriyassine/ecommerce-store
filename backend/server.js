854571dbae60a22eb8a5ddc67253759e309c88a4
eljabriyassine <yassinejabrio.2001@gmail.com>
2024-09-12 11:48:26 +0100

    feat: database setup

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
+import connectToMongoDB from "./lib/db.js";
 
 dotenv.config();
+
+connectToMongoDB();
 
 const PORT = process.env.PORT || 5000;
 
